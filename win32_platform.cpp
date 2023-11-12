83107b40d3dbbca8765552aba5b9c1021e7d34b4
Connor Panso <cpanso@outlook.com>
2023-11-11 21:55:20 -0500

    Initalized program and setup files to begin program

---
+++ win32_platform.cpp
@@ -0,0 +1,10 @@
+#include <windows.h>
+
+// entry point for graphical windows based application
+int WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nShowCmd) {
+	// create window class 
+
+	// register class
+
+	// create window
+}
