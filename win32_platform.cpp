9b4193b0c565a3f0504c3f448216bcfa886b3211
Connor Panso <cpanso@outlook.com>
2023-11-11 22:42:49 -0500

    Set up window in loop while game is rendering

--- win32_platform.cpp
+++ win32_platform.cpp
@@ -1,10 +1,48 @@
 #include <windows.h>
 
+bool running = true;
+
+LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	LRESULT result = 0;
+
+	switch (uMsg) {
+		case WM_CLOSE:
+		case WM_DESTROY: {
+			running = false;
+		} break;
+
+
+		default: {
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+	}
+	return result;
+}
+
 // entry point for graphical windows based application
-int WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nShowCmd) {
-	// create window class 
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	// Create Window Class
+	WNDCLASS window_class = {};
+	window_class.style = CS_HREDRAW | CS_VREDRAW;
+	window_class.lpszClassName = L"Game Window Class";
+	window_class.lpfnWndProc = window_callback;
 
-	// register class
+	// Register Class
+	RegisterClass(&window_class);
 
-	// create window
+	// Create Window
+	HWND window = CreateWindow(window_class.lpszClassName, L"My First Pong Game!", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+
+	while (running) {
+		//input 
+		MSG message;
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+		// simulate
+
+		// render
+	}
+
 }
